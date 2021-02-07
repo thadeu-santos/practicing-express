737dea4ee0b0e269e1fbd24103300938a4abe1f9
thadeu-santos <thadeulsantos@gmail.com>
2021-02-07 19:15:31 -0300

    adding nodemon package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send("Hello world!"));
+app.get('/contato', (req, res) => res.send(['contato1', 'contato2', 'conato3']))
 
 app.listen(8000, ()=> {
     console.log('O nome de Jesus é poderoso')
